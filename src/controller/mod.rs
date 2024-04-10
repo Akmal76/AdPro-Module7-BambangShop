ed7744181ae00887b472423832775df75181434b
Akmal76 <akmalramadhan76@gmail.com>
2024-04-10 23:12:31 +0700

    Implement subscribe function in Notification controller.

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -7,6 +7,6 @@ pub fn route_stage() -> AdHoc {
     return AdHoc::on_ignite("Initializing controller routes...", |rocket| async {
         rocket
             .mount("/product", routes![product::create, product::list, product::read, product::delete])
-            .mount("/notification", routes![])
+            .mount("/notification", routes![notification::subscribe])
     });
 }
--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -4,3 +4,11 @@ use rocket::serde::json::Json;
 use bambangshop::Result;
 use crate::model::subscriber::Subscriber;
 use crate::service::notification::NotificationService;
+
+#[post("/subscribe/<product_type>", data = "<subscriber>")]
+pub fn subscribe(product_type: &str, subscriber: Json<Subscriber>) -> Result<Created<Json<Subscriber>>> {
+    return match NotificationService::subscribe(product_type, subscriber.intro_inner()) {
+        Ok(f) => Ok(Created::new("/").body(Json::from(f))),
+        Err(e) => Err(e)
+    };
+}
