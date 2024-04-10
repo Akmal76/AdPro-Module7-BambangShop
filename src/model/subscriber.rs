85cd95e276b7b126ee739ab6c6348dfde02e3f25
Akmal76 <akmalramadhan76@gmail.com>
2024-04-11 00:23:41 +0700

    Implement update method in Subscriber model to send notification HTTP requests.

--- src/model/subscriber.rs
+++ src/model/subscriber.rs
@@ -11,3 +11,16 @@ pub struct Subscriber {
     pub url: String,
     pub name: String,
 }
+
+impl Subscriber {
+    #[tokio::main]
+    pub async fn update(&self, payload: Notification) {
+        REQWEST_CLIENT
+            .post(&self.url)
+            .header("Content-Type", "JSON")
+            .body(to_string(&payload).unwrap())
+            .send().await.ok();
+        log::warn_!("Sent {} notification of: [{}] {}, to: {}",
+            payload.status, payload.product_type, payload.product_title, self.url)
+    }
+}
