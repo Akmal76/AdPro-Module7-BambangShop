c2ce986456bb66aac739aac612c49dfe4ec3c30e
Akmal76 <akmalramadhan76@gmail.com>
2024-04-10 23:09:42 +0700

    Implement subscribe function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -10,5 +10,10 @@ use crate::repository::subscriber::SubscriberRepository;
 pub struct NotificationService;
 
 impl NotificationService {
-
+    pub fn subscribe(product_type: &str, subscriber: Subscriber) -> Result<Subscriber> {
+        let product_type_upper: String = product_type.to_uppercase();
+        let product_type_str: &str = product_type_upper.as_str();
+        let subscriber_result: Subscriber = SubscriberRepository::add(product_type_str, subscriber);
+        return Ok(subscriber_result);
+    }
 }
